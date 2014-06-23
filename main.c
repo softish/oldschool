bc9765c3c5b7194e16cff7a4484293ab4c7034f2
softish <lazar.h.klincov@live.se>
2014-06-23 16:38:07 +0200

    added makefile with anjuta ide, minor changes in main

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
-// 
+//
 #include "includes.h"
 
+void create_file(char filename[]); /// rename write_to_file + branch = filehandler
 
 int main(void)
 {
-	
+    create_file("filename");
 
 	return 0;
+}
+
+void create_file(char filename[])
+{
+    FILE* fp;
+    int i, j;
+
+    if((fp = fopen(filename, "w")) == NULL)
+        perror("open_file");
+
+    for(i = 0, j = 0; i < 20; i++, j++)
+    {
+        //if(j % 10 == 0)
+            fprintf(fp, "\n");  // trying to get newline
+            fprintf(fp, "%d", '\x0a');
+
+        fprintf(fp, "%d", 4);
+    }
+
 }
---
+++ makefile
@@ -0,0 +1,31 @@
+
+## Created by Anjuta
+
+CC = gcc
+CFLAGS = -g -Wall
+OBJECTS = main.o
+INCFLAGS = 
+LDFLAGS = -Wl,-rpath,/usr/local/lib
+LIBS = 
+
+all: main
+
+main: $(OBJECTS)
+	$(CC) -o main $(OBJECTS) $(LDFLAGS) $(LIBS)
+
+.SUFFIXES:
+.SUFFIXES:	.c .cc .C .cpp .o
+
+.c.o :
+	$(CC) -o $@ -c $(CFLAGS) $< $(INCFLAGS)
+
+count:
+	wc *.c *.cc *.C *.cpp *.h *.hpp
+
+clean:
+	rm -f *.o
+
+.PHONY: all
+.PHONY: count
+.PHONY: clean
+
